28635597b4c69a8ecef871cf47b38c150c61a061
Caroline <claversa11@gmail.com>
2024-04-17 14:49:30 -0400

    it made me do this to accept changes

--- finalProject/routes/countdownRoutes.js
+++ finalProject/routes/countdownRoutes.js
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-import { Router } from 'express';
-const router = Router();
-// Data file for the data functions
-import * as data from '../data/users.js'; // need stuff from users db
-
-router.route('/').get(async (req, res) => {
-    //code here for GET will render the home handlebars file
-
-    // get date of this user's next upcoming race from user info database
-
-    // render date and other info in pages/countdown/countdown.handlebars
-    // the otherCss is the other stylesheet that that page specifically uses so it should be IN THAT SAME FOLDER since it will be injected into the
-    // handlebars file in that folder
-
-    res.render("./countdown", { otherCss: "./public/countdown.css", title: "Race Day Countdown" });
-});
-
-
-//export router
-=======
 import { Router } from 'express';
 const router = Router();
 // Data file for the data functions
@@ -30,5 +9,4 @@ router.route('/').get(async (req, res) => {
 
 
 //export router
->>>>>>> Stashed changes
 export default router;
