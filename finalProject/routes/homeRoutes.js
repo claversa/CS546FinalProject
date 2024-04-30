ff8a69cbb76fa6d8bae97b3af4fae05901b5aee4
Ty Saito <tsaito@Tys-MacBook-Pro.local>
2024-04-30 19:56:43 -0400

    please work for the lboe of god i moved routes around

--- finalProject/routes/homeRoutes.js
+++ finalProject/routes/homeRoutes.js
@@ -1,14 +1,43 @@
 import { Router } from 'express';
 const router = Router();
 // Data file for the data functions
-import * as data from '../data/races.js'; // need stuff from race db
-import path from 'path';
+import * as races from '../data/races.js'; // need stuff from race db
 import * as help from "../helpers/helpers.js"
-import { error } from 'console';
+import * as data from '../data/users.js';
 
 router.route('/').get(async (req, res) => {
     //render the home handlebars file
     res.render('home', { title: "Homepage", user: req.session.user, error: "" }); // NO ERROR
+});
+
+router.route('/logout').get(async (req, res) => {
+    delete req.session.user
+    res.redirect('./home');
+});
+
+router.route('/login')
+  .get(async (req, res) => {
+    //render the home handlebars file
+    res.render('login', { title: "Login", user: req.session.user });
+  })
+  .post(async (req, res) => {
+    const loginInfo = req.body; // form info!
+    let username = loginInfo.username;
+    let password = loginInfo.password;
+    try {
+      let validation = await data.check(username, password)
+      if (validation) {
+        console.log(validation.darkmode)
+        req.session.user = { username: username, coloblind: validation.coloblind, darkmode: validation.darkmode };
+        // take user to homepage but now logged in
+        res.redirect('./home');
+      } else {
+        throw "Username or Password is incorrect"
+      }
+    }
+    catch (e) {
+      res.status(404).render('error', { title: "Error", class: "not-found", error: e.toString() });
+    }
 });
 
 router.route('/:raceId').get(async (req, res) => { // specific race
